3ef0bf958152807126d474116527b19c11ecc175
OXullo Intersecans <x@brainrapers.org>
2012-02-16 15:11:35 +0100

    Proper connection/disconnection handling

--- tests/u0net-tester.py
+++ tests/u0net-tester.py
@@ -11,15 +11,19 @@ from twisted.python import log
 
 
 class U0NetProto(basic.LineReceiver):
-    delimiter='\r\n'
+    delimiter = '\r\n'
+
+    def lineReceived(self, line):
+        log.msg('Ignoring incoming line: %s' % line)
 
     def connectionMade(self):
-        log.msg('Connection made')
+        log.msg('Connection established: %s' % self.transport.getPeer())
         self.factory.protos.append(self)
 
-    def clientConnectionLost(self, connector, reason):
+    def connectionLost(self, reason):
+        log.msg('Connection lost with %s' % self.transport.getPeer())
         self.factory.protos.remove(self)
-    
+
 
 class U0Net(protocol.ServerFactory):
     protocol = U0NetProto
@@ -27,26 +31,26 @@ class U0Net(protocol.ServerFactory):
 
     def __init__(self):
         self.__cascade()
-        
+
     def sendShit(self):
         if self.protos:
             mask = random.randrange(0, 32)
             lmask = []
             for i in xrange(5):
                 lmask.append('I' if mask & 1 << i else 'X')
-            
+
             log.msg('Sending %s (%d)' % (str(lmask), mask))
             for proto in self.protos:
                 proto.sendLine('%d' % mask)
-            
+
         self.__cascade()
-    
+
     def __cascade(self):
         reactor.callLater(random.uniform(0.01, 1.5), self.sendShit)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     log.startLogging(sys.stdout)
     reactor.listenTCP(10000, U0Net())
-    
+
     reactor.run()
